6105ae2888f4e756a2df0bc13c10164371427d95
Ernesto Riveiro <ernesto.ernzxr@gmail.com>
2024-02-12 22:53:32 -0300

    Feat: Created Header, Routes and Connection to Wallet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,64 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { DecimalPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { MatAnchor } from '@angular/material/button';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { WalletStore } from '@heavy-duty/wallet-adapter';
 import { HdWalletMultiButtonComponent } from '@heavy-duty/wallet-adapter-material';
+import { computedAsync } from 'ngxtension/computed-async';
+import { ShyftApiService } from './shyft-api.service';
 
 @Component({
   standalone: true,
-  imports: [RouterModule, HdWalletMultiButtonComponent],
-  selector: 'bob-root',
+  imports: [
+    HdWalletMultiButtonComponent,
+    RouterOutlet,
+    RouterLink,
+    DecimalPipe,
+    MatAnchor,
+  ],
+  selector: 'First-dApp-root',
   template: `
     <header class="px-16 pt-24 pb-8">
-      <h1 class="text-center text-5xl mb-4">My Bank</h1>
-      
-      <div class="flex justify-center">
+      <h1 class="text-center text-5xl mb-4">El Banco de Ernesto</h1>
+
+      <div class="mb-4 flex justify-center">
         <hd-wallet-multi-button></hd-wallet-multi-button>
       </div>
+
+      @if (account()) {
+        <div class="mb-4 top-4 left-4 flex justify-center items-center gap-2">
+          <img [src]="account()?.info?.image" class="w-8 h-8" />
+          <p class="text-2xl font-bold">{{ account()?.balance | number }}</p>
+        </div>
+        <div class="mb-4 top-4 left-4 flex justify-center items-center gap-2">
+          <p class="text-2xl font-bold">Rich Bro</p>
+        </div>
+      }
+
+      <nav>
+        <ul class="flex justify-center items-center gap-4">
+          <li>
+            <a [routerLink]="['']" mat-raised-button>Home</a>
+          </li>
+          <li>
+            <a [routerLink]="['settings']" mat-raised-button>Settings</a>
+          </li>
+        </ul>
+      </nav>
     </header>
 
-    <main></main>
+    <main>
+      <router-outlet></router-outlet>
+    </main>
   `,
 })
-export class AppComponent {}
+export class AppComponent {
+  private readonly _shyftApiService = inject(ShyftApiService);
+  private readonly _walletStore = inject(WalletStore);
+  private readonly _publicKey = toSignal(this._walletStore.publicKey$);
+  readonly account = computedAsync(
+    () => this._shyftApiService.getAccout(this._publicKey()?.toBase58()),
+    { requireSync: true },
+  );
+}
---
+++ src/app/settings-section.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bob-settings-section',
+  template: ` <section class="px-24 py-32 bg-white bg-opacity-5">
+    <p class="text-center text-3xl">Settings</p>
+  </section>`,
+  standalone: true,
+})
+export class SettingsSectionComponent {}
